ec1bce393badae9394849f8fd36cc3464e3404a1
Ricky Thomas <rjtom2@hotmail.com>
2024-10-17 19:35:43 -0400

    Add files via upload

---
+++ PetStore/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore;
+
+public class Product
+{
+
+    string? Name;
+    decimal Price;
+    int Quantity;
+    string? Description;
+
+
+
+
+    public class CatFood
+    {
+        double WeightPounds;
+        bool KittenFood;
+    }
+
+
+    public class DogLeash
+    {
+        int LengthInches;
+        string? Material;
+    }
+}
+    
---
+++ PetStore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using PetStore;
+
+Console.WriteLine("Press 1 to add a product");
+Console.WriteLine("Type 'exit' to quit");
+string userInput = Console.ReadLine();
+
+while (userInput.ToLower() != "exit")
+{
+    if (userInput.ToLower() == "1")
+    {
+        Product.DogLeash dogleash = new Product.DogLeash
+        {
+            Material = Console.ReadLine()
+        };
+    }
+    else 
+    Console.WriteLine("Press 1 to add a product");
+    Console.WriteLine("Type 'exit' to quit");
+    userInput = Console.ReadLine();
+}
+ 
